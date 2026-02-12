244237e1d3fbf23addb1f35d992b48d30bd5b090
zhongjianhao <v-zhongjianhao@immotors.com>
2026-02-12 17:39:54 +0800

    修改插件

--- api/content.rs
+++ api/content.rs
@@ -1,5 +1,5 @@
 use serde_json::json;
-use vercel_runtime::{run, String, Error, Request, Response, StatusCode};
+use vercel_runtime::{run, Error, Request, Response, StatusCode};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
--- api/index.rs
+++ api/index.rs
@@ -1,24 +1,17 @@
 use serde_json::json;
-use vercel_runtime::{run, String, Error, Request, Response, StatusCode};
+use vercel_runtime::{run, Error, Request, Response};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     run(handler).await
 }
 
-pub async fn handler(_req: Request) -> Result<Response<String>, Error> {
-    Ok(Response::builder()
-        .status(StatusCode::OK)
-        .header("Content-Type", "application/json")
-        .body(
-            json!({
-              "rust": "fanqie",
-              "message": "参数错误",
-              "data": {
-                  "content": "没有数据"
-              }
-            })
-            .to_string()
-            .into(),
-        )?)
+async fn handler(_req: Request) -> Result<Response<String>, Error> {
+    Response::json(&json!({
+        "rust": "fanqie",
+        "message": "参数错误",
+        "data": {
+            "content": "没有数据"
+        }
+    }))
 }
