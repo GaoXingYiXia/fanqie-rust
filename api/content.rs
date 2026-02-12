8f73f3ae7c4bcfc83a874626a0297f1b04733cfc
zhongjianhao <v-zhongjianhao@immotors.com>
2026-02-12 17:46:27 +0800

    修改文件

--- api/content.rs
+++ api/content.rs
@@ -1,21 +1,14 @@
 use serde_json::json;
-use vercel_runtime::{run, Error, Request, Response, StatusCode};
+use vercel_runtime::{run, Body, Error, Request, Response};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     run(handler).await
 }
 
-pub async fn handler(_req: Request) -> Result<Response<String>, Error> {
-    Ok(Response::builder()
-        .status(StatusCode::OK)
-        .header("Content-Type", "application/json")
-        .body(
-            json!({
-              "message": "Hello from Rust!",
-              "status": "success"
-            })
-            .to_string()
-            .into(),
-        )?)
+async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+  Response::json(&json!({
+      "message": "Hello from Rust!",
+      "status": "success"
+  }))
 }
--- api/index.rs
+++ api/index.rs
@@ -1,17 +1,15 @@
 use serde_json::json;
-use vercel_runtime::{run, Error, Request, Response};
+use vercel_runtime::{run, Body, Error, Request, Response};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     run(handler).await
 }
 
-async fn handler(_req: Request) -> Result<Response<String>, Error> {
-    Response::json(&json!({
-        "rust": "fanqie",
-        "message": "参数错误",
-        "data": {
-            "content": "没有数据"
-        }
-    }))
+async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+  Response::json(&json!({
+      "rust": "fanqie",
+      "message": "参数错误",
+      "data": { "content": "没有数据" }
+  }))
 }
